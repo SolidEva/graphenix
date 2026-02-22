44dd885f8f77b1539f94fca90a8c157d81fe2b1c
Eva Emmerich <eva@evaemmerich.com>
2026-02-22 14:51:09 +0100

    add device specific wrappers, makes creating regular builds easy

---
+++ tokay.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DEVICE=tokay
+
+# build grapheneos for tokay at a specific tagged release
+
+ if [ -z "$1" ]; then
+  echo "Usage: ./tokay.sh <grapheneos-release-tag-name>"
+  echo "the tags can be found here https://grapheneos.org/releases"
+  exit 1
+fi
+
+TAG=$1
+
+./clone.sh $TAG
+./build.sh $TAG $DEVICE
+
+mkdir -p grapheneos-$TAG/keys/
+cp -a keys/$DEVICE grapheneos-$TAG/keys/$DEVICE
+cd grapheneos-$TAG
+script/generate-release.sh $DEVICE $TAG
+cd ../
+echo "signed images available in grapheneos-${TAG}/releases/${TAG}/release-${DEVICE}-${TAG}"
